ad621ab0a85b01db1e351158fd909f5eeffc6383
Dheeraj Agrawal <dheeraja486@gmail.com>
2017-09-01 23:43:54 +0530

    df

--- untitled1/settings/base.py
+++ untitled1/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l3s+vc$g(o9avff6$)j0kgm02y&$21y=^mez(x3%pm*%9ux6if'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://logbook-semi.herokuapp.com/']
 
 
 # Application definition
